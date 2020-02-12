7643215bb212f7009967e3f12d14e4e74d77cccd
FranciscoContreras <contrerasfrancisco2010@gmail.com>
2020-02-11 16:17:57 -0800

    Coding challenge #2

--- the-basics/script.js
+++ the-basics/script.js
@@ -167,3 +167,49 @@ if (height || height === '' ) {
 if (height === "23") {
     console.log('The == operator does type coercion!'); 
 } */
+
+/*****************************
+ * * CODING CHALLENGE 2
+ */
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, 
+John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 
+and 123 points.
+
+1. Calculate the average score for each team
+
+2. Decide which teams wins in average (highest average score), and print the 
+winner to the console. Also include the average score in the output.
+
+3. Then change the scores to show different winners.
+ Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. 
+Like before, log the average winner to the console. 
+HINT: you will need the && operator to take the decision. 
+If you can't solve this one, just watch the solution, it's no problem :)
+
+5. Like before, change the scores to generate different winners, 
+keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
+
+// Calcualte averages
+
+let johnTeamAverage = (89 + 120 + 103) / 3; 
+
+let miketeamAverage = (116 + 94 + 123) / 3;
+
+let maryTeamAverage = (97 + 134 + 105) / 3;
+
+if (johnTeamAverage > miketeamAverage && johnTeamAverage > maryTeamAverage) {
+    console.log("John's team had the highest average with: " + johnTeamAverage); 
+} else if (miketeamAverage > johnTeamAverage && miketeamAverage > maryTeamAverage) {
+    console.log("Mike's team had the highest average with: " + miketeamAverage); 
+} else if (maryTeamAverage > johnTeamAverage && maryTeamAverage > miketeamAverage) {
+    console.log("Mary's team had the highest average with: " + maryTeamAverage); 
+} else {
+    console.log("All the teams had the same average.")
+}
