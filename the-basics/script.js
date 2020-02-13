12db857fb1001402d610ddc6f7284b111697600c
FranciscoContreras <contrerasfrancisco2010@gmail.com>
2020-02-12 20:00:02 -0800

    Coding Challenge 3: Arrays & Fuctions

--- the-basics/script.js
+++ the-basics/script.js
@@ -304,3 +304,45 @@ let isDesigner = john.indexOf('designer') === -1 ?
 
 console.log(isDesigner);  */
 
+/*****************************
+ * * CODING CHALLENGE 3
+ */
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). 
+He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, 
+and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+
+let total = [124, 48, 268]; 
+
+function tipCalculator(amount) {
+    if (amount < 50) {
+        return amount * .2; 
+    }
+    else if (amount >= 50 && amount < 200) {
+        return amount * .15;    
+    } else {
+        return amount * .10;
+    }
+}
+
+let tipAmounts = [tipCalculator(total[0]), 
+                  tipCalculator(total[1]), 
+                  tipCalculator(total[2])];
+
+let totalAmounts = [tipAmounts[0] + total[0], 
+                    tipAmounts[1] + total[1], 
+                    tipAmounts[2] + total[2]];
+
+console.log(tipAmounts, totalAmounts);
